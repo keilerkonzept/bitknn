e728441b8e10c86afbd5511315b7a48b7cff06a8
Sergey Grebenshchikov <sgreben@users.noreply.github.com>
2024-10-10 03:15:23 +0200

    lsh: only stop early for 1-NN

--- lsh/nearest_wide.go
+++ lsh/nearest_wide.go
@@ -13,9 +13,8 @@ func NearestWide(data [][]uint64, bucketIDs []uint64, buckets map[uint64]slice.I
 	exactBucket := buckets[xh]
 	numExamined := exactBucket.Length
 	nearestWideInBucket(data, exactBucket, k, x, &distances[0], &dataHeap)
-
-	// if the exact bucket already contains k neighbors, stop and return them
-	if dataHeap.Len() == k {
+	// stop early for 1-NN
+	if k == 1 && dataHeap.Len() == k {
 		return k, exactBucket.Length
 	}
 
